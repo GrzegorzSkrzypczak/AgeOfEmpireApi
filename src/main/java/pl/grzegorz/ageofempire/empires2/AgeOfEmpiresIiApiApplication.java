015c2f1adc1d0eabfa293fc0a9152ec91117df17
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-10-06 21:56:09 +0200

    init(aoea): init project

---
+++ src/main/java/pl/grzegorz/ageofempire/empires2/AgeOfEmpiresIiApiApplication.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.ageofempire.empires2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgeOfEmpiresIiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgeOfEmpiresIiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/grzegorz/ageofempire/empires2/AgeOfEmpiresIiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.ageofempire.empires2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeOfEmpiresIiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

b08b1833447462a855cc6d41bcc5e39828e80650
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-11-11 18:41:53 +0100

    fix(aoea): fixed get url addresin civilizations

--- src/main/java/pl/grzegorz/ageofempire/empires2/app_loader/repository/CivilisationResult.java
+++ src/main/java/pl/grzegorz/ageofempire/empires2/app_loader/repository/CivilisationResult.java
@@ -5,31 +5,16 @@ public class CivilisationResult {
 
     private int id;
     private String name;
-    //TODO załączyć adres url do proprtries
-    private String url = "https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/";
 
 
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public String getUrl() {
-        return url + id;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
 }
--- src/main/java/pl/grzegorz/ageofempire/empires2/app_loader/service/CivilizationTransformer.java
+++ src/main/java/pl/grzegorz/ageofempire/empires2/app_loader/service/CivilizationTransformer.java
@@ -1,5 +1,6 @@
 package pl.grzegorz.ageofempire.empires2.app_loader.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import pl.grzegorz.ageofempire.empires2.app_loader.repository.CivilisationResult;
 import pl.grzegorz.ageofempire.empires2.common.repository.Civilization;
@@ -10,8 +11,11 @@ import java.util.stream.Collectors;
 @Component
 public class CivilizationTransformer {
 
+    @Value("${civilizationcontroller.url}")
+    private String url;
+
     public Civilization transformToCivilization(CivilisationResult civilisationResult) {
-        return new Civilization(civilisationResult.getId(), civilisationResult.getName(), civilisationResult.getUrl());
+        return new Civilization(civilisationResult.getId(), civilisationResult.getName(), url + civilisationResult.getId());
     }
 
     public List<Civilization> transfromToCivilizationList(List<CivilisationResult> civilisationResultList) {
